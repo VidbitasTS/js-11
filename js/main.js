d25ecf048d23313a58907430af2507a3aaa3c18e
vidbitas <vidbitas@gmail.com>
2022-07-20 14:29:48 +0300

    testai

--- js/main.js
+++ js/main.js
@@ -7,9 +7,32 @@ let div = document.createElement('div');
 div.className = 'alert';
 
 // 3. Fill it with the content
-div.innerHTML = "<strong>Hi there!</strong> You've read an important message.";
-const str = "<strong>Hi there!</strong> You've read an important message.";
-
-div.append(str);
+//div.innerHTML = "<strong>Hi there!</strong> You've read an important message.";
+//const str = "<strong>Hi there!</strong> You've read an important message.";
 
 //div.append(str);
+const strongEl = document.createElement('strong');
+
+strongEl.textContent = 'Hi here ';
+
+div.append(strongEl, ' This is and alert');
+
+//div.textContent =|" This is and select";
+
+//document.body.appendChild(div);
+
+//div.append(str);
+
+const colorsListEl = document.getElementById('ul-color');
+console.log(colorsListEl);
+
+const sectionEl = colorsListEl.parentElement;
+console.log(sectionEl);
+
+const pEl = colorsListEl.nextElementSibling;
+console.log(pEl);
+
+pEl.style.fontSize = '50px';
+
+const secondEl = colorsListEl.children[1];
+secondEl.style.color = 'coral';
